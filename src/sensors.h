655413d0d5d2f2d3f88fa0dd8964861de091099c
Huỳnh Văn Khoa <huynhvankhoa260918@gmail.com>
2025-11-19 23:07:49 +0700

    Update sensors.h

--- src/sensors.h
+++ src/sensors.h
@@ -2,25 +2,28 @@
 #define  SENSORS_H
 
 struct SensorData {
-  float temperature;
-  float humidity;
-  float voltage;
-  float current;
-  float power;
-  float energy;
-  float frequency;
-  float pf;
+  float temperature; //Nhiệt độ (°C)
+  float humidity;    //Độ ẩm (%)
+  float voltage;     //Điện áp lưới (V)
+  float current;     //Dòng điện (A)
+  float power;       //Công suất tức thời (W)
+  float energy;      //Điện năng tiêu thụ (KWh)
+  float frequency;   //Tần số lưới (Hz)
+  float pf;          //Hệ số công suất (Power Factor)
 };
 
 extern volatile SensorData sensordata;
-//Cờ theo dõi
+//=================================================
+//Các cờ báo hiệu sự kiện (được set trong sensor.c)
+//=================================================
 extern volatile bool coTrom;
 extern volatile bool batDauMua;
 extern volatile bool hetMua;
 extern volatile bool matDien;
 extern volatile bool coDien;
-
-//Cam bien 
+//=======================
+//Hàm đọc và cập nhật biến 
+//======================== 
 void readDHT22();
 void readPZEM();
 void updateRain();
@@ -29,7 +32,9 @@ void checkIR();
 void updatePIR();
 void updateDisplay();
 void updateBlinkLED();
-//Khởi tạo cảm biến ( setupSensor )
+//============================================================
+//Khởi tạo tất cả các cảm biến và ngoại vi( gọi trong setup())
+//============================================================
 void initSensor();
 
 #endif
