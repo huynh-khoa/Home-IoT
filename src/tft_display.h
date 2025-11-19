cde6e7be614de3e328f9565593e9a03baef80ee6
Huỳnh Văn Khoa <huynhvankhoa260918@gmail.com>
2025-11-19 23:09:20 +0700

    Update tft_display.h

--- src/tft_display.h
+++ src/tft_display.h
@@ -5,19 +5,22 @@
 #include <Adafruit_ST7735.h>
 #include <SPI.h>
 
-// ===== Cấu hình chân TFT =====
+// ===================================================
+// Chân kết nối TFT ST7735 phần cứng cố định
 #define TFT_CS   32  
 #define TFT_RST  33
 #define TFT_DC   25
 #define TFT_SDA  27
 #define TFT_SCL  14
-
+// TFT toàn cục (khai báo ở .cpp)
 extern Adafruit_ST7735 tft;
-
-// ===== Các hàm hiển thị =====
-void initDisplay();
-void displayDHT(float temp, float humi);
-void displayPZEM(float voltage, float power);
+//========================================================
+//  Các hàm hiển thị trên màn hình TFT
+//========================================================
+void initDisplay();                                       //Khởi tạo màn hình 
+void displayDHT(float temp, float humi);                  //Cập nhật nhiệt độ + độ ẩm (khi thay đổi)
+void displayPZEM(float voltage, float power);             //Cập nhật trạng thái công suất & điện năng tiêu thụ
 void displayStatus(bool isRaining, int servoPos, bool rainSensorEnabled);
+//->Hiển thị: mưa/khô, phơi rút, cảm biến mưa ON/OFF
 
 #endif
